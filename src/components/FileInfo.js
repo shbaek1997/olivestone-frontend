7c807a06e0d2972de5fbfc3c40c8eb6d8e90f79c
shbaek1997 <shbaek1997@olivestonelab.com>
2022-11-15 18:23:57 +0900

    Feat: add change password button for file info component

--- src/components/FileInfo.js
+++ src/components/FileInfo.js
@@ -1,8 +1,9 @@
-const FileInfo = ({ originalName, _id, expireDate }) => {
+import styled from "styled-components";
+
+const FileInfo = ({ originalName, _id, expireDate, setPropsFunc }) => {
   const expireDateToString = expireDate.toString().slice(0, 10);
   const handleButtonClick = (event) => {
-    //여기서 아이디 값으로 api요청 수정..
-    console.log(_id);
+    setPropsFunc(_id, true);
   };
 
   return (
@@ -10,9 +11,26 @@ const FileInfo = ({ originalName, _id, expireDate }) => {
       <div>{_id}</div>
       <div>{originalName}</div>
       <div>{expireDateToString}</div>
-      <button onClick={handleButtonClick}>Change Password</button>
+      <StyledFileButton onClick={handleButtonClick}>
+        Change Password
+      </StyledFileButton>
     </>
   );
 };
 
 export default FileInfo;
+
+const StyledFileButton = styled.button`
+  width: 150px;
+  color: white;
+  background-color: black;
+  padding: 10px;
+
+  border-radius: 10px;
+  border: none;
+  font-weight: bold;
+  &:hover {
+    background-color: grey;
+    cursor: pointer;
+  }
+`;
