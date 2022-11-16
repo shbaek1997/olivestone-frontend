562d3d9cf39870250493f5320601af61b21c0280
shbaek1997 <shbaek1997@olivestonelab.com>
2022-11-16 11:22:06 +0900

    Feat: file modal form submission handling

--- src/components/FileModal.js
+++ src/components/FileModal.js
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 import { StyledForm, StyledInput, StyledButton } from "../style/style";
+import Api from "../utils/api";
+import useInput from "../hooks/useInput";
+import { errorHandler } from "../utils/error-handler";
 const FileModal = ({ isActive, fileId, setPropsFunc }) => {
-  const handleModalSubmit = (event) => {
-    event.preventDefault();
-    console.log(fileId);
+  const [filePassword, setFilePassword, handleChangeFilePassword] =
+    useInput("");
+  const [
+    fileRepeatPassword,
+    setFileRepeatPassword,
+    handleChangeFileRepeatPassword,
+  ] = useInput("");
+  const handleModalSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      console.log(fileId);
+      const api = Api();
+      console.log("passwords:", filePassword, fileRepeatPassword);
+      const response = await api.patch(
+        `files/${fileId}`,
+        { filePassword, fileRepeatPassword },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      setFilePassword("");
+      setFileRepeatPassword("");
+      const { originalName } = response?.data;
+      alert(`${originalName} 파일 비밀번호가 성공적으로 변경되었습니다.`);
+      setPropsFunc("", false);
+    } catch (error) {
+      errorHandler(error);
+    }
   };
   return (
     <StyledFileModal
@@ -12,9 +40,19 @@ const FileModal = ({ isActive, fileId, setPropsFunc }) => {
     >
       <StyledForm onSubmit={handleModalSubmit}>
         <label>Enter new file password</label>
-        <StyledInput type={"password"} required></StyledInput>
+        <StyledInput
+          type={"password"}
+          required
+          value={filePassword}
+          onChange={handleChangeFilePassword}
+        ></StyledInput>
         <label>Confirm new file password</label>
-        <StyledInput type={"password"} required></StyledInput>
+        <StyledInput
+          type={"password"}
+          required
+          value={fileRepeatPassword}
+          onChange={handleChangeFileRepeatPassword}
+        ></StyledInput>
         <StyledButton>Submit</StyledButton>
       </StyledForm>
       <StyledButton
