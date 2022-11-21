f5971382367fdb940d1f78b13c0ead3d7c947993
shbaek1997 <shbaek1997@olivestonelab.com>
2022-11-21 18:31:59 +0900

    Feat: add go-back-to-login button, see all files button, logout button to download page

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import useInput from "../hooks/useInput";
+import checkLogin from "../utils/checkLogin";
 import {
   StyledContainer,
   StyledFormContainer,
@@ -15,7 +17,27 @@ import { errorHandler } from "../utils/error-handler";
 //download page
 export function Download() {
   //set file ID, download file password state and onChange handlers.
-
+  /////////////
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogout = () => {
+    //clear any jwt
+    sessionStorage.clear();
+    // change login state, change upload success to remove file id
+    setIsLoggedIn(false);
+  };
+  const setLogInValue = async () => {
+    const checkValue = await checkLogin();
+    //return true if logged in, return false if not logged in
+    setIsLoggedIn(checkValue);
+    // set loading false
+    setIsLoading(false);
+  };
+  // call setLogInValue once when page is first loaded
+  useEffect(() => {
+    setLogInValue();
+  }, []);
+  ///////////////
   const downloadFileId = new URLSearchParams(window.location.search).get(
     "fileId"
   );
@@ -53,7 +75,9 @@ export function Download() {
     }
   };
 
-  return (
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
     <StyledContainer>
       <StyledHeader>Upload and Download</StyledHeader>
       <StyledFormContainer>
@@ -77,13 +101,33 @@ export function Download() {
           ></StyledInput>
           <StyledButton type="submit">Download File</StyledButton>
         </StyledForm>
-        <StyledButton
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Go back to Upload
-        </StyledButton>
+        {isLoggedIn ? (
+          <>
+            <StyledButton
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Go back to Upload
+            </StyledButton>
+            <StyledButton
+              onClick={() => {
+                navigate("/files");
+              }}
+            >
+              See all files
+            </StyledButton>
+            <StyledButton onClick={handleLogout}>Log out</StyledButton>
+          </>
+        ) : (
+          <StyledButton
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Go back to login
+          </StyledButton>
+        )}
       </StyledFormContainer>
     </StyledContainer>
   );
