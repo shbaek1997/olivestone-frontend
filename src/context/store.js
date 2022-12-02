af2c53b467a0f2d5c3d16c6883623700c6b6d8b5
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-02 11:20:42 +0900

    Feat:add dark mode reducer to redux store

--- src/context/store.js
+++ src/context/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import modalReducer from "./modalSlice";
 import fileReducer from "./fileSlice";
+import darkModeReducer from "./darkModeSlice";
 //combine reducers from slices to form one reducer
 const reducer = {
   auth: authReducer,
   modal: modalReducer,
   files: fileReducer,
+  darkMode: darkModeReducer,
 };
 //configure store
 const store = configureStore({
