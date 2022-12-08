8ff7ccb4ae392de1417286022db3d97a9b2f19b6
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-08 13:14:04 +0900

    Fix: chanage error handler function

--- src/utils/error-handler.js
+++ src/utils/error-handler.js
@@ -2,18 +2,16 @@
 export const errorHandler = async (error) => {
   //if not axios error, just alert error.message
   if (!error.response) {
-    alert(error.message);
-    return;
+    return error.message;
   }
   const { data } = error.response;
   // specific for downloading improper file ID format
   if (error.response.data instanceof Blob) {
     let { reason } = JSON.parse(await data.text());
-    alert(reason);
-    return;
+    return reason;
   }
   //if it is axios error ...
 
   const { reason } = data;
-  alert(reason);
+  return reason;
 };
