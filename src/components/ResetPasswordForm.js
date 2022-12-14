10ea7282636bb2116f772107e0c02c8b3ab34ef2
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-14 14:11:48 +0900

    Feat: add reset password form component

---
+++ src/components/ResetPasswordForm.js
@@ -0,0 +1,95 @@
+import { useDispatch } from "react-redux";
+import useInput from "../hooks/useInput";
+import Api from "../utils/api";
+import { errorHandler } from "../utils/error-handler";
+import { StyledForm, StyledButton, StyledInput } from "../style/style";
+import { turnAlertOff, turnAlertOn } from "../context/modalSlice";
+import { useNavigate } from "react-router-dom";
+//download page
+export const ResetPasswordForm = ({ isTokenValid, user }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail, handleChangeEmail] = useInput("");
+  const [password, setPassword, handleChangePassword] = useInput("");
+  const [passwordRepeat, setPasswordRepeat, handleChangePasswordRepeat] =
+    useInput("");
+  //set download password state, onChange handler
+  // download submit logic
+  const handleResetSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      //validate id and password format
+      ///////////
+      //   await downloadFileSchema.validate({
+      //     fileId,
+      //     filePassword: downloadPassword,
+      //   });
+      ////////
+      //request post api for downloading the file, response should be blob
+      const api = Api();
+      dispatch(turnAlertOn("Trying to send reset email..."));
+      const response = await api.post("users/reset-password", { email });
+      dispatch(turnAlertOff());
+      const { data } = response;
+      const message = data.result;
+      dispatch(turnAlertOn(message));
+      //   const response = await api.
+      setEmail("");
+    } catch (error) {
+      const message = await errorHandler(error);
+      dispatch(turnAlertOn(message));
+    }
+  };
+
+  const handleResetPasswordSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(user);
+      const userId = user._id;
+      const api = Api();
+      await api.patch(`users/${userId}/reset-password/password`, {
+        password,
+        passwordRepeat,
+      });
+
+      dispatch(turnAlertOn("비밀번호가 새로 설정되었습니다."));
+      setPassword("");
+      setPasswordRepeat("");
+      navigate("/login");
+    } catch (error) {
+      const message = await errorHandler(error);
+      dispatch(turnAlertOn(message));
+    }
+  };
+
+  return isTokenValid ? (
+    <StyledForm method="post" onSubmit={handleResetPasswordSubmit}>
+      <label htmlFor="password-input">Password</label>
+      <StyledInput
+        id="password-input"
+        type="password"
+        onChange={handleChangePassword}
+        value={password}
+      ></StyledInput>
+      <label htmlFor="password-repeat-input">Confirm Password</label>
+      <StyledInput
+        id="password-repeat-input"
+        type="password"
+        onChange={handleChangePasswordRepeat}
+        value={passwordRepeat}
+      ></StyledInput>
+      <StyledButton type="submit">Reset Password</StyledButton>
+    </StyledForm>
+  ) : (
+    <StyledForm method="post" onSubmit={handleResetSubmit}>
+      <label htmlFor="email-input">Email</label>
+      <StyledInput
+        id="email-input"
+        type="email"
+        onChange={handleChangeEmail}
+        value={email}
+      ></StyledInput>
+      <StyledButton type="submit">Reset Password</StyledButton>
+    </StyledForm>
+  );
+};
